e1ec3625fb1042dbb0af30e0284f368a68d7c121
Zachary Lehmann <zlehmann@gmail.com>
2018-12-09 16:47:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
-
+  get '/' do
+    "Hello, World!"
+  end
+  # They should tell you how to run this outside of the ide!
 
 end
